a91951c986259feb38671c259e1e511b29b9f447
SefryantoV <97575726+SefryantoV@users.noreply.github.com>
2022-02-08 10:16:10 +0700

    objek pertemuan ke 3

---
+++ whenkotlin.kt
@@ -0,0 +1,14 @@
+fun main ()
+{
+    var index = 1
+
+    var greeting = "Halo"
+    while ( index <= 5 )
+    {
+        if (index == 4)
+        {
+            println(greeting)
+        }
+    }
+
+}
---
+++ whilecontinue.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    var index = 1
+
+    var greeting = "hallo"
+    while (index <= 5)
+    {
+        if (index == 4)
+        {
+            println(greeting)
+            break
+        }
+        index++
+    }
+
+
+}
